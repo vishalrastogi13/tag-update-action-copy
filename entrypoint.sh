515f7cf104d7bed40348336d664833ee449591eb
vishalrastogi13 <95530856+vishalrastogi13@users.noreply.github.com>
2022-07-06 13:46:35 +0530

    Changed from api call to git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,25 +4,25 @@ set -o pipefail
 
 # config
 CUSTOM_VERSION=${custom_tag}
-REPOSITORY=${repository}
-initial_version='v1.0.0'
+INITIAL_VERSION='v1.0.0'
+
 
 echo "*** CONFIGURATION ***"
 echo -e "\tCUSTOM_VERSION: ${custom_tag}"
 
 #get highest tag number, and add 1.0.0 if doesn't exist
-git fetch --tags
-# This suppress an error occurred when the repository is a complete one.
-git fetch --prune --unshallow || true
+git fetch --all --tags
+# # This suppress an error occurred when the repository is a complete one.
+# git fetch --prune --unshallow || true
 CURRENT_VERSION=''
-CURRENT_VERSION=$(git describe --abbrev=0 --tags || true)
+CURRENT_VERSION=$(git tag | sort -V | tail -1)
 
 echo "current version ${CURRENT_VERSION}"
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-    echo "No Tag as of now, creating a new Tag ${initial_version}"
-    NEW_TAG=$initial_version
+    echo "No Tag as of now, creating a new Tag ${INITIAL_VERSION}"
+    NEW_TAG=$INITIAL_VERSION
 else
     echo "Current Major Version is ${CURRENT_VERSION} & Custom specified version is ${CUSTOM_VERSION}"
     CURRENT_VERSION="${CURRENT_VERSION#?}"
@@ -69,8 +69,9 @@ tag_commit=$(git rev-list -n 1 $NEW_TAG)
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$NEW_TAG
+    echo "No new commits since previous tag. Skipping creating a new tag..."
+    # Set job output as new tag
+    echo ::set-output name=new-tag::$CURRENT_VERSION
     exit 0
 fi
 
@@ -78,34 +79,42 @@ fi
 git tag $NEW_TAG
 
 # push new tag ref to github
-dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-full_name=$REPOSITORY
+git push origin $NEW_TAG
+
+#get highest tag number
+git fetch --all --tags
+if [ $(git tag | sort -V | tail -1) = $NEW_TAG ]; then
+    echo ::set-output name=new-tag::$NEW_TAG
+    exit 0
+fi
+    
+
+# dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+# full_name=$GITHUB_REPOSITORY
+
 # git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
 
-git_refs_url='https://api.github.com/repos/'$REPOSITORY'/git/refs'
+# echo "$dt: **pushing tag $NEW_TAG to repo $full_name"
 
-echo "$dt: **pushing tag $NEW_TAG to repo $full_name"
-echo "git ref url :: $git_refs_url"
+# git_refs_response=$(
+# curl -s -X POST $git_refs_url \
+# -H "Authorization: token $GITHUB_TOKEN" \
+# -d @- << EOF
 
-git_refs_response=$(
-curl -s -X POST $git_refs_url \
--H "Authorization: token $GITHUB_TOKEN" \
--d @- << EOF
+# {
+#   "ref": "refs/tags/$NEW_TAG",
+#   "sha": "$commit"
+# }
+# EOF
+# )
 
-{
-  "ref": "refs/tags/$NEW_TAG",
-  "sha": "$commit"
-}
-EOF
-)
+# git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
 
-git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-echo "::set-output name=tag::$NEW_TAG"
+# echo "::debug::${git_refs_response}"
+# if [ "${git_ref_posted}" = "refs/tags/${NEW_TAG}" ]; then
+#     # Set job output as new tag
+#     echo ::set-output name=new-tag::$NEW_TAG
+#     exit 0
 
-echo "::debug::${git_refs_response}"
-if [ "${git_ref_posted}" = "refs/tags/${NEW_TAG}" ]; then
-  exit 0
-else
-  echo "::error::Tag was not created properly."
-  exit 1
-fi
+echo "::error::Tag was not created properly."
+exit 1
